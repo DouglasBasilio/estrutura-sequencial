2d839b07137d662b6c60e59c6a8ce9439f468ab6
DouglasBasilio <basilio.douglas@yahoo.com.br>
2020-11-03 20:42:09 -0300

    Exercícios sobre Estrutura Sequencial (entrada, processamento, saída)

---
+++ src/exec_01.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+//Faça um programa para ler dois valores inteiros, 
+//e depois mostrar na tela a soma desses números com uma mensagem explicativa
+
+public class exec_01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		
+		int soma = n1 + n2;
+		
+		System.out.println("SOMA = " + soma);
+		sc.close();
+	}
+
+}
